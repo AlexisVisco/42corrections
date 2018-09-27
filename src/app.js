f3c7b52a3ebd6c0212b4638c71c9ad9338f1a320
alexis@batch.com <alexis@batch.com>
2018-09-27 15:57:14 +0200

    update, may work, need more api keys :c

--- src/app.js
+++ src/app.js
@@ -1,57 +1,85 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
 const requests = require("./requests");
 const users = require("./users");
 const balancer = require("./balancer");
+const stats = require("stats-lite")
 
 
-balancer.init().then(async () => {
+async function retrieveCorrectors(correctorsOccurence, teamId) {
+    let hasFeedback = false
+    let team = await requests.teams(teamId);
+    if (team["scale_teams"]) {
+        if (team["scale_teams"].length !== 0) hasFeedback = true
+        for (let feedback of team["scale_teams"]) {
+            let login = feedback.corrector.login
+            if (!correctorsOccurence[login]) correctorsOccurence[login] = 1
+            else correctorsOccurence[login] = correctorsOccurence[login] + 1
+            // console.log("   -", login, correctorsOccurence[login])
+        }
+    }
+    return hasFeedback
+}
+
+function printInfo({numArray, retrys, array, numOfProjects}) {
+    console.log()
+    console.log("Numbers of corrections    : ", stats.sum(numArray));
+    console.log("Average per correctors    : ", stats.mean(numArray));
+    console.log("Maybe friends correctors  : ", stats.percentile(numArray, 0.85));
+    console.log("Average of retry          : ", stats.mean(retrys))
+
+    array.sort((a, b) => {
+        return b.count - a.count
+    })
+
+    console.log()
+    console.log("Friends correctors : ")
+
+    array.forEach(e => {
+        let p = (e.count / numOfProjects) * 100
+        if (e.count >= stats.percentile(numArray, 0.85)) console.log(" -", e.login, p.toPrecision(2) + "%")
+    })
+}
+
+async function retrieveUserStats(user) {
 
     let correctorsOccurence = {}
-    let user = users.list(9, 2017).find(e => e.login === "xamartin")
 
-    console.log("Target: ", user.login)
+    let numOfProjects = 0
+    let retrys = []
 
-    // id, occurence, final_mark, current_team_id, status, validated?, project.slug
     let projects = user["projects_users"].filter(e => e["cursus_ids"].includes(1))
 
-    for (let project of projects.filter(p => p["validated?"])) {
-        console.log(project.project.slug)
-        await requests.sleep(100)
-        let team = await requests.teams(project["current_team_id"]);
-        if (team["scale_teams"]) {
-            for (let feedback of team["scale_teams"]) {
-                if (!correctorsOccurence[feedback.corrector.login]) correctorsOccurence[feedback.corrector.login] = 1
-                else correctorsOccurence[feedback.corrector.login] = correctorsOccurence[feedback.corrector.login] + 1
-                console.log("   -", feedback.corrector.login, correctorsOccurence[feedback.corrector.login])
-            }
+    let projectsValidated = projects.filter(p => p["validated?"])
+    for (let project of projectsValidated) {
+        let sessionProject = await requests.project(project.id)
+        retrys.push(sessionProject.teams.length)
+        for (let teamInfo of sessionProject.teams) {
+            await requests.sleep(100)
+            if (await retrieveCorrectors(correctorsOccurence, teamInfo.id)) numOfProjects++
         }
     }
 
-    let toArray = Object.keys(correctorsOccurence).map(e => ({login: e, count: correctorsOccurence[e]}))
-    toArray.sort((a, b) => {
-        return b.count - a.count
-    })
-    console.log(toArray)
-    // team["scale_teams"].forEach(feedback => {
-    //     feedback["corrector"].forEach(corrector => {
-    //         console.log("  ",corrector.login)
-    //         if (!correctorsOccurence[corrector.login]) correctorsOccurence[corrector.login] = 1
-    //         else correctorsOccurence[corrector.login] = correctorsOccurence[corrector.login] + 1
-    //     })
-    // })
-})
+    let array = Object.keys(correctorsOccurence).map(e => ({login: e, count: correctorsOccurence[e]}))
+    let numArray = array.map(e => e.count)
 
-// console.log(correctorsOccurence)
-//
-// // //let project = await requests.project(748105)
-// // let team = await requests.teams(projects[2]["current_team_id"]);
-// //
-// // team["scale_teams"].forEach(e => {
-// //     console.log(e["corrector"]);
-// // })
-//
-// //console.log(JSON.stringify(team, null, 2))
-// //users.createUsersData(9, 2017).then(() => console.log("Yeah"))
-// //requests.user("edevos").then(e=> console.log(JSON.stringify(e)))
-// //setInterval(setupToken, 1000 * 30)
-// })
-// ;
+    return {login: user.login, array, numArray, numOfProjects, retrys}
+}
+
+balancer.init().then(async function () {
+    let userCorrections = {}
+    let listUsers = users.list(9, 2017)
+    let index = 1
+
+    for (let u of listUsers) {
+        process.stdout.write(u.login + "\t\t" +  index + "/" +  listUsers.length)
+        userCorrections[u.login] = await retrieveUserStats(u)
+        process.stdout.write("\tOK\n")
+        index++
+    }
+    const adapter = new FileSync(`db/corrections_9_2017.json`)
+    const db = low(adapter)
+
+    db.defaults(userCorrections).write()
+})
