2011560d37b9ad720f428ee42a441afc0fafd0ca
alexis@batch.com <alexis@batch.com>
2018-09-28 17:01:37 +0200

    add 9_2017

--- src/app.js
+++ src/app.js
@@ -37,8 +37,8 @@ function printInfo({numArray, retrys, array, numOfProjects}) {
     console.log("Friends correctors : ")
 
     array.forEach(e => {
-        let p = (e.count / numOfProjects) * 100
-        if (e.count >= stats.percentile(numArray, 0.85)) console.log(" -", e.login, p.toPrecision(2) + "%")
+        let p = (e.count / stats.sum(numArray)) * 100
+        if (e.count >= stats.percentile(numArray, 0.85)) console.log(" -", e.login, p.toPrecision(2) + "%", e.count, "times")
     })
 }
 
@@ -48,12 +48,14 @@ async function retrieveUserStats(user) {
 
     let numOfProjects = 0
     let retrys = []
+    let sessionsProject = []
 
     let projects = user["projects_users"].filter(e => e["cursus_ids"].includes(1))
 
     let projectsValidated = projects.filter(p => p["validated?"])
     for (let project of projectsValidated) {
         let sessionProject = await requests.project(project.id)
+        sessionsProject.push(sessionProject)
         retrys.push(sessionProject.teams.length)
         for (let teamInfo of sessionProject.teams) {
             await requests.sleep(100)
@@ -64,27 +66,43 @@ async function retrieveUserStats(user) {
     let array = Object.keys(correctorsOccurence).map(e => ({login: e, count: correctorsOccurence[e]}))
     let numArray = array.map(e => e.count)
 
-    return {login: user.login, array, numArray, numOfProjects, retrys}
+    return {login: user.login, array, numArray, numOfProjects, retrys, projects: sessionsProject}
 }
 
-balancer.init().then(async function () {
-    let userCorrections = {}
-    let listUsers = users.list(9, 2017)
-    let index = 1
+// balancer.init().then(async function () {
+//     let userCorrections = {}
+//     let listUsers = users.list(9, 2017)
+//     let index = 1
+//
+//     const adapter = new FileSync(`db/corrections_9_2017.json`)
+//     const db = low(adapter)
+//
+//     db.defaults({corrections: []}).write()
+//
+//     for (let u of listUsers) {
+//         process.stdout.write(printf('%-10s %-8s', u.login,  index + "/" +  listUsers.length))
+//         db.get('corrections')
+//             .push(await retrieveUserStats(u))
+//             .write()
+//         process.stdout.write(printf("  OK\n"))
+//         index++
+//     }
+//
+//     db.defaults(userCorrections).write()
+// })
 
-    const adapter = new FileSync(`db/corrections_9_2017.json`)
-    const db = low(adapter)
+const adapter = new FileSync(`db/corrections_9_2017.json`)
+const db = low(adapter).getState()
 
-    db.defaults({corrections: []}).write()
+let list = db.corrections.filter(e => e.array.length > 0)
+list.forEach((e) => e.array.sort((a, b) => {
+    return b.count - a.count
+}))
 
-    for (let u of listUsers) {
-        process.stdout.write(printf('%-10s %-8s', u.login,  index + "/" +  listUsers.length))
-        db.get('corrections')
-            .push(await retrieveUserStats(u))
-            .write()
-        process.stdout.write(printf("  OK\n"))
-        index++
-    }
+list.sort((a, b) => b.array[0].count - a.array[0].count)
 
-    db.defaults(userCorrections).write()
+list.forEach((e,i) => {
+    console.log("\n-------------------\n")
+    console.log(i+1, e.login)
+    printInfo(e)
 })
